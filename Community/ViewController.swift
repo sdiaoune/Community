b40db9d808c970a58924fb9b99fd3bd1371a37bc
sdiaoune <soyadiaoune@gmail.com>
2017-01-21 04:19:49 -0500

    initial
    initial

---
+++ Community/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Community
+//
+//  Created by Soya Diaoune on 1/21/17.
+//  Copyright © 2017 Soya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World!")
+        print("1,2,3")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
